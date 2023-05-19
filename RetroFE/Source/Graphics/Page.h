82755fb3abf815f8e526812a588e387ab7d31b67
inigomonyota <96964704+inigomonyota@users.noreply.github.com>
2023-05-19 16:45:16 -0400

    Update Page.h

--- RetroFE/Source/Graphics/Page.h
+++ RetroFE/Source/Graphics/Page.h
@@ -150,7 +150,7 @@ public:
     ScrollingList* getPlaylistMenu();
     void setPlaylistMenu(ScrollingList*);
     bool playlistExists(std::string);
-	void Page::setSelectedItem();
+    void setSelectedItem();
 
 private:
     void playlistChange();
