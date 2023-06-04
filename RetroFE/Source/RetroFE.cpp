7c239cebfaec5a4dd4d3baa3ab3d2b10b27367b9
Tobias Contreras <tobias.ca@gmail.com>
2023-06-04 15:39:40 -0700

    Fix lastplayed update in lastplayed (#47)
    * don't cycle playlist if on "street fighter and capcom fighters" or "street fighter"
    
    * lower errors to warnings in logs
    
    * fix saving to playlist from lastplayed playlist

--- RetroFE/Source/RetroFE.cpp
+++ RetroFE/Source/RetroFE.cpp
@@ -1233,8 +1233,7 @@ bool RetroFE::run( )
                 config_.getProperty( "lastPlayedSkipCollection", lastPlayedSkipCollection );
                 config_.getProperty( "lastplayedSize", size );
 
-                if (!isInAttractModeSkipPlaylist(currentPage_->getPlaylistName()) &&
-                    nextPageItem_->collectionInfo->name != lastPlayedSkipCollection)
+                if (nextPageItem_->collectionInfo->name != lastPlayedSkipCollection)
                 {
                     cib.updateLastPlayedPlaylist(currentPage_->getCollection(), nextPageItem_, size); // Update last played playlist if not currently in the skip playlist (e.g. settings)
                     // with new sort by last played return to first
