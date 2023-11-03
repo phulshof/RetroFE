f9adef94fa5e3924c4219e3bf935c88472eb1edc
aidenjbass <moshidosh83@gmail.com>
2023-11-03 16:36:20 +0000

    Init apple silicon

--- RetroFE/CMake/FindGlib2.cmake
+++ RetroFE/CMake/FindGlib2.cmake
@@ -3,33 +3,33 @@ FILE(TO_CMAKE_PATH "${GLIB2_ROOT}" TRY2_DIR)
 FILE(GLOB GLIB2_ROOT ${TRY1_DIR} ${TRY2_DIR})
 
 FIND_PATH(GLIB_glib_2_INCLUDE_DIR glib.h
-                                  PATHS ${GLIB2_ROOT}/include ${GLIB2_ROOT}/include/glib-2.0 /usr/local/include/glib-2.0 /usr/include/glib-2.0 /opt/local/include/glib-2.0
+                                  PATHS /opt/homebrew/Cellar/glib/2.78.1/include/glib-2.0 ${GLIB2_ROOT}/include ${GLIB2_ROOT}/include/glib-2.0 /usr/local/include/glib-2.0 /usr/include/glib-2.0 /opt/local/include/glib-2.0
                                   ENV INCLUDE DOC "Directory containing glib.h include file")
 
 FIND_PATH(GLIB_glibconfig_2_INCLUDE_DIR glibconfig.h
-                                        PATHS ${GLIB2_ROOT}/include ${GLIB2_ROOT}/include/glib-2.0 ${GLIB2_ROOT}/lib/include ${GLIB2_ROOT}/lib/glib-2.0/include /usr/local/include/glib-2.0 /usr/include/glib-2.0 /usr/lib/glib-2.0/include /usr/local/lib/glib-2.0/include /opt/local/lib/glib-2.0/include
+                                        PATHS /opt/homebrew/Cellar/glib/2.78.1/lib/glib-2.0/include ${GLIB2_ROOT}/include ${GLIB2_ROOT}/include/glib-2.0 ${GLIB2_ROOT}/lib/include ${GLIB2_ROOT}/lib/glib-2.0/include /usr/local/include/glib-2.0 /usr/include/glib-2.0 /usr/lib/glib-2.0/include /usr/local/lib/glib-2.0/include /opt/local/lib/glib-2.0/include
                                         ENV INCLUDE DOC "Directory containing glibconfig.h include file")
 
-FIND_LIBRARY(GLIB_glib_2_LIBRARY NAMES glib-2.0
-                                 PATHS ${GLIB2_ROOT}/bin ${GLIB2_ROOT}/win32/bin ${GLIB2_ROOT}/lib ${GLIB2_ROOT}/win32/lib /usr/local/lib /usr/lib /opt/local/lib
+FIND_LIBRARY(GLIB_glib_2_LIBRARY NAMES glib-2.0 libglib-2.0
+                                 PATHS /opt/homebrew/Cellar/glib/2.78.1/lib ${GLIB2_ROOT}/bin ${GLIB2_ROOT}/win32/bin ${GLIB2_ROOT}/lib ${GLIB2_ROOT}/win32/lib /usr/local/lib /usr/lib /opt/local/lib
                                  ENV LIB
                                  DOC "glib library to link with"
                                  NO_SYSTEM_ENVIRONMENT_PATH)
 
-FIND_LIBRARY(GLIB_gmodule_2_LIBRARY NAMES gmodule-2.0
-                                    PATHS ${GLIB2_ROOT}/bin ${GLIB2_ROOT}/win32/bin ${GLIB2_ROOT}/lib ${GLIB2_ROOT}/win32/lib /usr/local/lib /usr/lib /opt/local/lib
+FIND_LIBRARY(GLIB_gmodule_2_LIBRARY NAMES gmodule-2.0 libgmodule-2.0
+                                    PATHS /opt/homebrew/Cellar/glib/2.78.1/lib ${GLIB2_ROOT}/bin ${GLIB2_ROOT}/win32/bin ${GLIB2_ROOT}/lib ${GLIB2_ROOT}/win32/lib /usr/local/lib /usr/lib /opt/local/lib
                                     ENV LIB
                                     DOC "gmodule library to link with"
                                     NO_SYSTEM_ENVIRONMENT_PATH)
 
-FIND_LIBRARY(GLIB_gobject_2_LIBRARY NAMES gobject-2.0
-                                    PATHS ${GLIB2_ROOT}/bin ${GLIB2_ROOT}/win32/bin ${GLIB2_ROOT}/lib ${GLIB2_ROOT}/win32/lib /usr/local/lib /usr/lib /opt/local/lib
+FIND_LIBRARY(GLIB_gobject_2_LIBRARY NAMES gobject-2.0 libgobject-2.0
+                                    PATHS /opt/homebrew/Cellar/glib/2.78.1/lib ${GLIB2_ROOT}/bin ${GLIB2_ROOT}/win32/bin ${GLIB2_ROOT}/lib ${GLIB2_ROOT}/win32/lib /usr/local/lib /usr/lib /opt/local/lib
                                     ENV LIB
                                     DOC "gobject library to link with"
                                     NO_SYSTEM_ENVIRONMENT_PATH)
 
-FIND_LIBRARY(GLIB_gthread_2_LIBRARY NAMES gthread-2.0
-                                    PATHS ${GLIB2_ROOT}/bin ${GLIB2_ROOT}/win32/bin ${GLIB2_ROOT}/lib ${GLIB2_ROOT}/win32/lib /usr/local/lib /usr/lib /opt/local/lib
+FIND_LIBRARY(GLIB_gthread_2_LIBRARY NAMES gthread-2.0 libgthread-2.0
+                                    PATHS /opt/homebrew/Cellar/glib/2.78.1/lib ${GLIB2_ROOT}/bin ${GLIB2_ROOT}/win32/bin ${GLIB2_ROOT}/lib ${GLIB2_ROOT}/win32/lib /usr/local/lib /usr/lib /opt/local/lib
                                     ENV LIB
                                     DOC "gthread library to link with"
                                     NO_SYSTEM_ENVIRONMENT_PATH)
